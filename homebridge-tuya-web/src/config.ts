272c7681cae5f270e79776d9ab9aa8adcccef540
Riccardo Marino <rmfalco89@gmail.com>
2023-03-02 10:24:42 -0800

    first commit of modified plugin homebridge-tuya-web

---
+++ homebridge-tuya-web/src/config.ts
@@ -0,0 +1,35 @@
+import { PlatformConfig } from "homebridge";
+import { TuyaPlatform } from "./api/platform";
+import { TuyaDeviceType } from "./api/response";
+
+export type TuyaDeviceDefaults = {
+  id: string;
+  device_type: TuyaDeviceType;
+  min_temper: string | number;
+  max_temper: string | number;
+  current_temperature_factor: string | number;
+  target_temperature_factor: string | number;
+  dimmer_characteristics: "Brightness"[];
+  fan_characteristics: "Speed"[];
+  light_characteristics: ("Brightness" | "Color" | "Color Temperature")[];
+  cover_characteristics: "Stop"[];
+  min_brightness: string | number;
+  max_brightness: string | number;
+  min_kelvin: string | number;
+  max_kelvin: string | number;
+};
+
+type Config = {
+  options?: {
+    username?: string;
+    password?: string;
+    countryCode?: string;
+    platform?: TuyaPlatform;
+    pollingInterval?: number;
+    authTokenWaitTime?: number;
+  };
+  defaults?: Partial<TuyaDeviceDefaults>[];
+  scenes?: boolean | string[];
+};
+
+export type TuyaWebConfig = PlatformConfig & Config;
